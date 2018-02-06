04909b77d6285a48a1a81cb2371a41cbc4e9a576
panpan <panp85@163.com>
2018-02-06 16:24:43 +0800

    增加ftp server功能

---
+++ app/src/main/java/com/android/zedL03/swiftp/gui/MainActivity_Ftp.java
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2013 Pieter Pareit.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * Contributors:
+ * Pieter Pareit - initial API and implementation
+ ******************************************************************************/
+
+package com.android.zedL03.swiftp.gui;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import net.vrallev.android.cat.Cat;
+
+import com.android.zedL03.swiftp.App;
+import com.android.zedL03.BuildConfig;
+import com.android.zedL03.swiftp.FsSettings;
+import com.android.zedL03.R;
+
+/**
+ * This is the main activity for swiftp, it enables the user to start the server service
+ * and allows the users to change the settings.
+ */
+public class MainActivity_Ftp extends AppCompatActivity{
+
+    final static int PERMISSIONS_REQUEST_CODE = 12;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Cat.d("created");
+        setTheme(FsSettings.getTheme());
+        super.onCreate(savedInstanceState);
+
+        if (VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+        }
+
+        if (App.isFreeVersion() && App.isPaidVersionInstalled()) {
+            Cat.d("Running demo while paid is installed");
+            AlertDialog ad = new AlertDialog.Builder(this)
+                    .setTitle(R.string.demo_while_paid_dialog_title)
+                    .setMessage(R.string.demo_while_paid_dialog_message)
+                    .setPositiveButton(getText(android.R.string.ok),
+                            (d, w) -> {
+                                finish();
+                            })
+                    .create();
+            ad.show();
+        }
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new PreferenceFragment())
+                .commit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != PERMISSIONS_REQUEST_CODE) {
+            Cat.e("Unhandled request code");
+            return;
+        }
+        Cat.d("permissions: " + permissions.toString());
+        Cat.d("grantResults: " + grantResults.toString());
+        if (grantResults.length > 0) {
+            // Permissions not granted, close down
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Unable to proceed without the needed permissions, shutting down", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_feedback) {
+            String to = "pieter.pareit@gmail.com";
+            String subject = "FTP Server feedback";
+            String message = "Device: " + Build.MODEL + "\n" +
+                    "Android version: " + VERSION.RELEASE + "-" + VERSION.SDK_INT + "\n" +
+                    "Application: " + BuildConfig.APPLICATION_ID + " (" + BuildConfig.FLAVOR + ")\n" +
+                    "Application version: " + BuildConfig.VERSION_NAME + " - " + BuildConfig.VERSION_CODE + "\n" +
+                    "Feedback: \n_";
+
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+            emailIntent.setType("message/rfc822");
+
+            try {
+                startActivity(emailIntent);
+                Toast.makeText(this, R.string.use_english, Toast.LENGTH_LONG).show();
+            } catch (ActivityNotFoundException exception) {
+                Toast.makeText(this, R.string.unable_to_start_mail_client, Toast.LENGTH_LONG).show();
+            }
+
+        } else if (item.getItemId() == R.id.action_about) {
+            startActivity(new Intent(this, AboutActivity.class));
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/android/zedL03/swiftp/locale/EditActivity.java
@@ -0,0 +1,114 @@
+/*
+Copyright 2016-2017 Pieter Pareit
+
+This file is part of SwiFTP.
+
+SwiFTP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SwiFTP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SwiFTP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.android.zedL03.swiftp.locale;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.twofortyfouram.locale.sdk.client.ui.activity.AbstractAppCompatPluginActivity;
+
+import net.vrallev.android.cat.Cat;
+
+import com.android.zedL03.swiftp.FsSettings;
+import com.android.zedL03.R;
+
+import static com.android.zedL03.swiftp.locale.SettingsBundleHelper.BUNDLE_BOOLEAN_RUNNING;
+import static com.android.zedL03.swiftp.locale.SettingsBundleHelper.generateBundle;
+import static com.android.zedL03.swiftp.locale.SettingsBundleHelper.getBundleRunningState;
+
+/**
+ * Created by ppareit on 29/04/16.
+ */
+public class EditActivity extends AbstractAppCompatPluginActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(FsSettings.getTheme());
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.locale_edit_layout);
+
+        CharSequence callingApplicationLabel = null;
+        try {
+            PackageManager pm = getPackageManager();
+            ApplicationInfo ai = pm.getApplicationInfo(getCallingPackage(), 0);
+            callingApplicationLabel = pm.getApplicationLabel(ai);
+        } catch (final PackageManager.NameNotFoundException e) {
+            Cat.e("Calling package couldn't be found%s", e); //$NON-NLS-1$
+        }
+        if (null != callingApplicationLabel) {
+            setTitle(callingApplicationLabel);
+        }
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setSubtitle(R.string.swiftp_name);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean isBundleValid(@NonNull Bundle bundle) {
+        return SettingsBundleHelper.isBundleValid(bundle);
+    }
+
+    @Override
+    public void onPostCreateWithPreviousResult(@NonNull Bundle previousBundle,
+                                               @NonNull String previousBlurb) {
+        if (!isBundleValid(previousBundle)) {
+            Cat.e("Invalid bundle received, repairing to default");
+            previousBundle = generateBundle(this, false);
+        }
+        boolean running = previousBundle.getBoolean(BUNDLE_BOOLEAN_RUNNING);
+        RadioButton radioButton =
+                (RadioButton) findViewById(running ? R.id.radio_server_running :
+                                R.id.radio_server_stopped);
+        radioButton.setChecked(true);
+    }
+
+    @Nullable
+    @Override
+    public Bundle getResultBundle() {
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_server_state_group);
+        int checkedId = radioGroup.getCheckedRadioButtonId();
+        boolean running = (checkedId == R.id.radio_server_running);
+
+        return generateBundle(this, running);
+    }
+
+    @NonNull
+    @Override
+    public String getResultBlurb(@NonNull Bundle bundle) {
+        boolean running = getBundleRunningState(bundle);
+        return running ? "Running" : "Stopped";
+    }
+}
